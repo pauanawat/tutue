17914eb27a90e89a68d275d40359d9a6355fe7d2
toeyuk <76154052+toeyuk@users.noreply.github.com>
2021-01-08 14:34:47 +0700

    Add files via upload

---
+++ MyCouse.js
@@ -0,0 +1,13 @@
+const head = document.getElementById("h-1");
+
+function tap(){
+    
+    head.style.color = "#00BFFF"
+    head.style.borderBottomStyle = "solid"
+    head.style.borderBlockEndColor = "#00BFFF"
+    head.style.borderBottomWidth = "2px"
+    
+    head.style.opacity = 1
+}
+
+
